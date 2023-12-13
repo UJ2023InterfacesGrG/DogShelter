345ca11c7dc3de22ecb8cc800ee03a2499faa3c7
Piotr Wojcieszak <piotrekw1002@gmail.com>
2023-12-14 00:42:21 +0100

    Naprawiłem linki do styli i dodałem routing do każdej strony

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,46 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home');
 });
 
+/* GET walk page. */
+router.get('/walk', function(req, res, next) {
+  res.render('chcesz_wyprowadzic');
+});
 
-// Endpoint to Get a list of users
-router.get('/getDogs', function(req, res){
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+/* GET mywalks page. */
+router.get('/myWalks', function(req, res, next) {
+  res.render('mywalks');
+});
+
+/* GET found page. */
+router.get('/found', function(req, res, next) {
+  res.render('ostatnio_znalezione');
+});
+
+/* GET success page. */
+router.get('/success', function(req, res, next) {
+  res.render('spacer_udany');
+});
+
+/* GET reserveWalk page. */
+router.get('/reserveWalk', function(req, res, next) {
+  res.render('zarezerwuj_spacer');
+});
+
+// Endpoint to Get a list of dogs
+router.get('/ourDogs', function(req, res){
   fs.readFile(__dirname + "/" + "data/data.json", 'utf8', function(err, data){
     try {
       // Parse the JSON data
