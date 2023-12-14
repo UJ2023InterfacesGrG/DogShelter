952168899dd3feb14b574966404128b5b4247705
Stiopa866 <39134953+Stiopa866@users.noreply.github.com>
2023-12-14 14:20:05 +0100

    Squashed commit of the following:
    commit 46f3818a1e3f68928863f04842617920e5ffbc2c
    Author: Piotr Wojcieszak <piotrekw1002@gmail.com>
    Date:   Thu Dec 14 14:18:19 2023 +0100
    
        Commit przed branchem
    
    commit fe94a2996159a5eabfa6649a8f6fb83fd4991d56
    Author: Piotr Wojcieszak <piotrekw1002@gmail.com>
    Date:   Thu Dec 14 14:12:16 2023 +0100
    
        Dodano przykładowe dane dla reserwacji spacerów
    
    commit daa431a0b77ee297af50bd76e4044dafca9740d4
    Author: Piotr Wojcieszak <piotrekw1002@gmail.com>
    Date:   Thu Dec 14 14:03:11 2023 +0100
    
        Zmiana struktury danych json, dodany routing /home
    
    commit 345ca11c7dc3de22ecb8cc800ee03a2499faa3c7
    Author: Piotr Wojcieszak <piotrekw1002@gmail.com>
    Date:   Thu Dec 14 00:42:21 2023 +0100
    
        Naprawiłem linki do styli i dodałem routing do każdej strony
    
    commit 0784c5f502c634c3a42bdf11ab35e5964dd16856
    Author: Piotr Wojcieszak <piotrekw1002@gmail.com>
    Date:   Thu Dec 14 00:29:19 2023 +0100
    
        Dodano wczytanie danych psów z pliku JSON i wyświetlenie na stronie

---
+++ routes/index.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home');
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home');
+});
+
+/* GET walk page. */
+router.get('/walk', function(req, res, next) {
+  res.render('chcesz_wyprowadzic');
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+/* GET mywalks page. */
+router.get('/myWalks', function(req, res, next) {
+  res.render('mywalks');
+});
+
+/* GET found page. */
+router.get('/found', function(req, res, next) {
+  res.render('ostatnio_znalezione');
+});
+
+/* GET success page. */
+router.get('/success', function(req, res, next) {
+  res.render('spacer_udany');
+});
+
+/* GET reserveWalk page. */
+router.get('/reserveWalk', function(req, res, next) {
+  res.render('zarezerwuj_spacer');
+});
+
+// Endpoint to Get a list of dogs
+router.get('/ourDogs', function(req, res){
+  fs.readFile(__dirname + "/" + "data/dogs.json", 'utf8', function(err, data){
+    try {
+      // Parse the JSON data
+      const jsonData = JSON.parse(data);
+      console.log(jsonData.dog1)
+      // Pass the 'dogs' array to the EJS template
+      res.render('ourdogs', { dogs: jsonData });
+    } catch (error) {
+      console.error('Error parsing JSON:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+})
+
+module.exports = router;
